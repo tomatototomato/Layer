62d4058e6d1584497b674ce3354fb857076edee6
tomatototomato <mugi.mugi.7714@gmail.com>
2025-03-30 07:56:54 +0900

    Initial Commit

---
+++ Layer/LayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayerApp.swift
+//  Layer
+//
+//  Created by Tsumugi on 2025/03/30.
+//
+
+import SwiftUI
+
+@main
+struct LayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LayerTests/LayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LayerTests.swift
+//  LayerTests
+//
+//  Created by Tsumugi on 2025/03/30.
+//
+
+import Testing
+@testable import Layer
+
+struct LayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
